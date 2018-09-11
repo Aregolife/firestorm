1b0edf8d4c7f75ce8fe43d86cc6c38403421553b
ericflarsen <32423746+ericflarsen@users.noreply.github.com>
2018-09-11 14:30:38 -0500

    Update aregolife.js

--- cart/aregolife.js
+++ cart/aregolife.js
@@ -1,6 +1,8 @@
-$("#ctl00_ContentPlaceHolder1_chkSameAsShipping").change(function(){
-   if(this.checked){ $('#ctl00_ContentPlaceHolder1_lblCountry').parent().hide();} 
-   else {$('#ctl00_ContentPlaceHolder1_lblCountry').parent().show(); 
-       $("#aspnetForm").trigger("reset");
-   }
-});
+(function($){ 
+    $("#ctl00_ContentPlaceHolder1_chkSameAsShipping").change(function(){
+        if(this.checked){ $('#ctl00_ContentPlaceHolder1_lblCountry').parent().hide();} 
+        else {$('#ctl00_ContentPlaceHolder1_lblCountry').parent().show(); 
+            $("#aspnetForm").trigger("reset");
+        }
+    })
+})(jQuery);
